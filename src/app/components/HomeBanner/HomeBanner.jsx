b6339622e28a81925440a9d641fb8e3eb3a85ff2
Tahsin974 <tahsinislam974@gmail.com>
2025-06-16 17:43:06 +0600

    home banner

--- src/app/components/HomeBanner/HomeBanner.jsx
+++ src/app/components/HomeBanner/HomeBanner.jsx
@@ -12,6 +12,25 @@ export default function HomeBanner() {
       <div className="carousel ">
         <div id="slide1" className="carousel-item relative w-full">
           <Image src={banner1} width={1140} height={600} alt="banner" />
+          <div className="w-1/2 top-0 h-full absolute bg-gradient-to-r from-black/80 text-white flex items-center">
+            <div className="m-20 absolute  space-y-6">
+              <h1 className="text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex gap-3.5">
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-red-500 border-red-500 text-white shadow-none">
+                  Discover More
+                </button>
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-transparent border-white text-white shadow-none">
+                  More Projects
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="absolute  right-10 bottom-5 space-x-6 flex -translate-y-1/2 transform justify-between ">
             <a
               href="#slide6"
@@ -29,6 +48,25 @@ export default function HomeBanner() {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <Image src={banner2} width={1140} height={600} alt="banner" />
+          <div className="w-1/2 top-0 h-full absolute bg-gradient-to-r from-black/80 text-white flex items-center">
+            <div className="m-20 absolute  space-y-6">
+              <h1 className="text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex gap-3.5">
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-red-500 border-red-500 text-white shadow-none">
+                  Discover More
+                </button>
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-transparent border-white text-white shadow-none">
+                  More Projects
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="absolute right-10 bottom-5 space-x-6 flex -translate-y-1/2 transform justify-between">
             <a
               href="#slide1"
@@ -46,6 +84,25 @@ export default function HomeBanner() {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <Image src={banner3} width={1140} height={600} alt="banner" />
+          <div className="w-1/2 top-0 h-full absolute bg-gradient-to-r from-black/80 text-white flex items-center">
+            <div className="m-20 absolute  space-y-6">
+              <h1 className="text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex gap-3.5">
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-red-500 border-red-500 text-white shadow-none">
+                  Discover More
+                </button>
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-transparent border-white text-white shadow-none">
+                  More Projects
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="absolute right-10 bottom-5 space-x-6 flex -translate-y-1/2 transform justify-between">
             <a
               href="#slide2"
@@ -63,6 +120,25 @@ export default function HomeBanner() {
         </div>
         <div id="slide4" className="carousel-item relative w-full">
           <Image src={banner4} width={1140} height={600} alt="banner" />
+          <div className="w-1/2 top-0 h-full absolute bg-gradient-to-r from-black/80 text-white flex items-center">
+            <div className="m-20 absolute  space-y-6">
+              <h1 className="text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex gap-3.5">
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-red-500 border-red-500 text-white shadow-none">
+                  Discover More
+                </button>
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-transparent border-white text-white shadow-none">
+                  More Projects
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="absolute right-10 bottom-5 space-x-6 flex -translate-y-1/2 transform justify-between">
             <a
               href="#slide3"
@@ -80,6 +156,25 @@ export default function HomeBanner() {
         </div>
         <div id="slide5" className="carousel-item relative w-full">
           <Image src={banner5} width={1140} height={600} alt="banner" />
+          <div className="w-1/2 top-0 h-full absolute bg-gradient-to-r from-black/80 text-white flex items-center">
+            <div className="m-20 absolute  space-y-6">
+              <h1 className="text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex gap-3.5">
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-red-500 border-red-500 text-white shadow-none">
+                  Discover More
+                </button>
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-transparent border-white text-white shadow-none">
+                  More Projects
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="absolute right-10 bottom-5 space-x-6 flex -translate-y-1/2 transform justify-between">
             <a
               href="#slide4"
@@ -97,6 +192,25 @@ export default function HomeBanner() {
         </div>
         <div id="slide6" className="carousel-item relative w-full">
           <Image src={banner6} width={1140} height={600} alt="banner" />
+          <div className="w-1/2 top-0 h-full absolute bg-gradient-to-r from-black/80 text-white flex items-center">
+            <div className="m-20 absolute  space-y-6">
+              <h1 className="text-6xl font-bold">
+                Affordable <br /> Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="flex gap-3.5">
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-red-500 border-red-500 text-white shadow-none">
+                  Discover More
+                </button>
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-lg bg-transparent border-white text-white shadow-none">
+                  More Projects
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="absolute right-10 bottom-5 space-x-6 flex -translate-y-1/2 transform justify-between">
             <a
               href="#slide5"
